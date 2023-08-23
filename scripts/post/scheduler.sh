5933919ce4527c79d257e8dc3abfc9bf494226fe
ATamblingPoder <117522662+ATamblingPoder@users.noreply.github.com>
2023-08-23 19:55:51 +0530

    build-ublue

--- scripts/post/scheduler.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell this script to exit if there are any errors.
-set -oue pipefail
-
-sudo wget https://copr.fedorainfracloud.org/coprs/kylegospo/system76-scheduler/repo/fedora-$(rpm -E %fedora)/kylegospo-system76-scheduler-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_kylegospo-system76-scheduler.repo
-sudo rpm-ostree install system76-scheduler
