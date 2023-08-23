7b42fcf8bee7bcbabf7b3709b38f730cdaec2032
ATamblingPoder <117522662+ATamblingPoder@users.noreply.github.com>
2023-08-23 19:25:28 +0530

    Adding deps for scheduler

---
+++ scripts/pre/scheduler-prep.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+set -oue pipefail
+
+rpm-ostree install rust cargo clang clang-devel pipewire-devel rust-pipewire-devel pkgconf-pkg-config
+echo $(just --version)
